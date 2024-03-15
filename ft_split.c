5a8681a70d759bec69f758774a7c6e1bb260f0c8
SamueleGalli <samuele.aggeggio@gmail.com>
2024-03-15 13:38:08 +0100

    libft

---
+++ ft_split.c
@@ -0,0 +1,176 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sgalli <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/21 13:43:55 by sgalli            #+#    #+#             */
+/*   Updated: 2022/11/11 11:51:51 by sgalli           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <stdio.h>
+#include "libft.h"
+
+static int	ft_count(char const *s, char c)
+{	
+	int	i;
+	int	sn;
+
+	i = 0;
+	sn = 0;
+	while (s[i] != 0)
+	{
+		if (s[i] != c)
+		{
+			sn++;
+			while (s[i] != c && s[i] != '\0')
+				i++;
+		}
+		if (s[i] != '\0')
+			i++;
+	}
+	return (sn);
+}
+
+static size_t	ft_superstrlen(const char *s, char c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != c && s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+char	**ft_split(const char *s, char c)
+{
+	int		i;
+	int		ns;
+	int		j;
+	char	**str;
+
+	j = 0;
+	i = 0;
+	ns = ft_count(s, c);
+	str = (char **)malloc((ns + 1) * sizeof(char *));
+	while (ns--)
+	{
+		while (s[i] != 0)
+		{
+			if (s[i] != c)
+			{
+				str[j] = ft_substr(s, i, ft_superstrlen(&s[i], c));
+				i = i + ft_superstrlen(&s[i], c);
+				break ;
+			}
+			i++;
+		}
+		j++;
+	}
+	str[j] = 0;
+	return (str);
+}
+/*
+void	ft_print_result(char const *s)
+{
+	int		len;
+
+	len = 0;
+	while (s[len])
+		len++;
+	write(1, s, len);
+}
+
+int		main(int argc, const char *argv[])
+{
+	char	**tabstr;
+	int		i;
+	int		arg;
+
+	if (argc == 1)
+		return (0);
+	i = 0;
+	if ((arg = atoi(argv[1])) == 1)
+	{
+		if (!(tabstr = ft_split("          ", ' ')))
+			ft_print_result("NULL");
+		else
+		{
+			while (tabstr[i] != NULL)
+			{
+				ft_print_result(tabstr[i]);
+				write(1, "\n", 1);
+				i++;
+			}
+		}
+	}
+	else if (arg == 2)
+	{
+		if (!(tabstr = ft_split("lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse", ' ')))
+			ft_print_result("NULL");
+		else
+		{
+			while (tabstr[i] != NULL)
+			{
+				ft_print_result(tabstr[i]);
+				write(1, "\n", 1);
+				i++;
+			}
+		}
+	}
+	else if (arg == 3)
+	{
+		if (!(tabstr = ft_split("   lorem   ipsum dolor     sit amet, consectetur   adipiscing elit. Sed non risus. Suspendisse   ", ' ')))
+			ft_print_result("NULL");
+		else
+		{
+			while (tabstr[i] != NULL)
+			{
+				ft_print_result(tabstr[i]);
+				write(1, "\n", 1);
+				i++;
+			}
+		}
+	}
+	else if (arg == 4)
+	{
+		if (!(tabstr = ft_split("lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultricies diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi.", 'i')))
+			ft_print_result("NULL");
+		else
+		{
+			while (tabstr[i] != NULL)
+			{
+				ft_print_result(tabstr[i]);
+				write(1, "\n", 1);
+				i++;
+			}
+		}
+	}
+	else if (arg == 5)
+	{
+		if (!(tabstr = ft_split("lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultricies diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi.", 'z')))
+			ft_print_result("NULL");
+		else
+		{
+			while (tabstr[i] != NULL)
+			{
+				ft_print_result(tabstr[i]);
+				write(1, "\n", 1);
+				i++;
+			}
+		}
+	}
+	else if (arg == 6)
+	{
+		if (!(tabstr = ft_split("", 'z')))
+			ft_print_result("NULL");
+		else
+			if (!tabstr[0])
+				ft_print_result("ok\n");
+	}
+	return (0);
+}*/
